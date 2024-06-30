4134a0cdf4b44032e0e52ee7c23bf9de8e4ca892
IsaqueRaFiPetri <irfpetri@gmail.com>
2024-06-30 16:21:33 -0300

    Item Pegável Feito

--- Projeto Dragao do Mar/Assets/Scripts/ObjectsInteraction/GrabableObject.cs
+++ Projeto Dragao do Mar/Assets/Scripts/ObjectsInteraction/GrabableObject.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GrabableObject : InteractableObject
 {
+    public GameObject item;
+    public GameObject temParent;
+    public Transform guide;
+
     protected override void Interact()
     {
 
@@ -11,12 +15,28 @@ public class GrabableObject : InteractableObject
     // Start is called before the first frame update
     void Start()
     {
-        
+        item.GetComponent<Rigidbody>().useGravity = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnMouseDown()
+    {
+        item.GetComponent<Rigidbody>().useGravity = false;
+        item.GetComponent<Rigidbody>().isKinematic = true;
+        item.transform.position = guide.transform.position;
+        item.transform.rotation = guide.transform.rotation;
+        item.transform.parent = guide.transform.parent;
+    }
+    void OnMouseUp()
+    {
+        item.GetComponent<Rigidbody>().useGravity = true;
+        item.GetComponent<Rigidbody>().isKinematic = false;
+        item.transform.position = guide.transform.position;
+        item.transform.parent = null;
     }
 }
