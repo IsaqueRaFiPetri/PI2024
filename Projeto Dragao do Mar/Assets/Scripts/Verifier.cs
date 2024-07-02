5d4dcd81c7401f5ace0857893f6f9682cc1d80da
IsaqueRaFiPetri <irfpetri@gmail.com>
2024-07-02 16:34:27 -0300

    Verify

---
+++ Projeto Dragao do Mar/Assets/Scripts/Verifier.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+enum verifier
+{
+    none, wrong, correct
+}
+public class Verifier : MonoBehaviour
+{
+    public static Verifier Instance;
+    public int correctDropdowns, currentDropdowns;
+    public UnityEvent Correct, Wrong, Verify;
+
+    public void Awake()
+    {
+        currentDropdowns = 0;
+        Instance = this;
+    }
+    public void VerifyCall()
+    {
+        Verify.Invoke();
+    }
+    public void verifier()
+    {
+        if (currentDropdowns >= correctDropdowns)
+        {
+            Correct.Invoke();
+        }
+        else
+        {
+            Wrong.Invoke();
+        }
+    }
+}
