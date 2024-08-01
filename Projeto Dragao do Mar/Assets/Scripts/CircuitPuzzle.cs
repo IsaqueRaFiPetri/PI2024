3039b9ac426f8615a9ae37c2c582fc465eda648c
IsaqueRaFiPetri <irfpetri@gmail.com>
2024-08-01 08:57:09 -0300

    Troca do Puzzle
    Puzzle na Prisão -> https://www.youtube.com/watch?v=bG-nZiJnx6s&list=PLrw9uFU6NhfOnEhqi7DRbx-eXiTrmbVGD&index=1

---
+++ Projeto Dragao do Mar/Assets/Scripts/CircuitPuzzle.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircuitPuzzle : MonoBehaviour
+{
+    public GameObject[] points;
+    public GameObject[] ropes;
+    bool isAnyRopeCrossing;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
